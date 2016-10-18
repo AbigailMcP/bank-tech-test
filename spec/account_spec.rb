7122d3c5f82b7134041490f306e26ff71a2164f1
Abigail <abigail.mcphillips@gmail.com>
2016-10-18 10:42:46 +0100

    implements deposit function to increase balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,10 @@ describe Account do
     expect(account.balance).to eq 0
   end
 
+  describe '#deposit' do
+    it 'increases the balance by the deposit amount' do
+      expect{ account.deposit(1000) }.to change{ account.balance }.by 1000
+    end
+  end
+
 end
